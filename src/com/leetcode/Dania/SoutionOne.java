4dc46f50ccc3798acec128bf45ed30d516104edc
Dania <daniatub8@gmail.com>
2022-05-25 12:00:20 +0300

    //Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.

--- src/com/leetcode/Dania/SoutionOne.java
+++ src/com/leetcode/Dania/SoutionOne.java
@@ -1,6 +1,39 @@
 package com.leetcode.Dania;
+
+import java.util.Stack;
+
 //Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 public class SoutionOne
 {
+    Stack myStack;
+
+    public SoutionOne() {
+        myStack = new Stack(); //define the stack
+    }
+
+    public void push(int val) {
+        myStack.push(val);
+    }
+
+    public void pop() {
+        myStack.pop();
+    }
+
+    public int top() {
+        int x = (int) myStack.pop(); //take the first elementm in the stack ..
+        myStack.push(x); //then push it again
+        return x;
+    }
+
+    public int getMin()
+    {
+        int min = Integer.MAX_VALUE;
+        for(Object elem : myStack)
+        {
+            min = Math.min((Integer) elem, min);
+        }
+        return min;
+    }
+
 
 }
